b9424d74e0889b53b5dc5e2a35ca31e04300e462
Rajni Gupta <rajnigupta2901@gmail.com>
2022-02-25 17:15:31 +0530

    gif added

--- TASK-1/Method-1/Objectdetection_v3.py
+++ TASK-1/Method-1/Objectdetection_v3.py
@@ -6,9 +6,9 @@ thres = 0.5                                 # Threshold to detect object
 nms_threshold = 0.2                         # (0.1 to 1) 1 means no suppress , 0.1 means high suppress 
 
 cap = cv2.VideoCapture('FC.mp4')
-cap.set(cv2.CAP_PROP_FRAME_WIDTH,280)       #width 
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT,120)      #height 
-cap.set(cv2.CAP_PROP_BRIGHTNESS,150)        #brightness 
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,280)       # width 
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,120)      # height 
+cap.set(cv2.CAP_PROP_BRIGHTNESS,150)        # brightness 
 
 classNames = []
 with open('coco.names','r') as f:
